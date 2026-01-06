e669035e6eda7db89e2ffeddbbec8626144a96e1
mikesav1 <kimesav@gmail.com>
2026-01-06 13:20:24 +0100

    Add @ alias for CRA/CRACO build

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 module.exports = {
   webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
     configure: (webpackConfig) => {
+      // Keep build stable: remove ForkTsChecker if present
       webpackConfig.plugins = (webpackConfig.plugins || []).filter(
         (p) => !(p && p.constructor && p.constructor.name === "ForkTsCheckerWebpackPlugin")
       );
